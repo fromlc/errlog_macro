a8ea861f2458e0e12502f2ba5d5900177ec57714
L Carver <wifihum@gmail.com>
2023-04-08 08:56:20 -0700

    add function name to ERRLOG output

--- source/errlog.h
+++ source/errlog.h
@@ -14,8 +14,8 @@
 
 #ifdef USE_ERRLOG
 #include <iostream>
-#define ERRLOG(msg) std::cerr << msg << '\n' \
-    << __FILE__ << ", line " << __LINE__ \
+#define ERRLOG(msg) std::cerr << msg << '\n' << __FILE__ \
+    << "\nin function " << __FUNCTION__ << "() at line " << __LINE__ \
     << "\nfile last modified " << __TIMESTAMP__ << "\n\n"
 #else
 #define ERRLOG(msg)
